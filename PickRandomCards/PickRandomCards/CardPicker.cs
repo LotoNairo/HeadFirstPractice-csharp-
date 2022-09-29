b2072d1902ffb646eeaa746a01f295df711c402b
Stranger <andrey.zanko.01@mail.ru>
2022-09-29 21:00:12 +0300

    add project PickRandomCards

---
+++ PickRandomCards/PickRandomCards/CardPicker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards) 
+        {
+            string[] PickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++) 
+            {
+                PickedCards[i] = RandomValue() + "of" + RandomSuit();
+            }
+            return PickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+
+            int value = random.Next(1, 5);// Возвращает случаенйное число от одного до 4
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            return "Diamonds";
+
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
---
+++ PickRandomCards/PickRandomCards/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+            //Если строка может быть преобразована в int то итог преобразования сохранаяетмя в переменной numderOfCards
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Please inter a valid numder.");
+            }
+        }
+    }
+}
